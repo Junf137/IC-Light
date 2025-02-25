79f89d2fc120b9f641269aaad7ab877ded33606a
Junfeng Lei <3086638517@qq.com>
2025-02-25 14:23:05 -0500

    [feat] add compute_canada.sh

---
+++ compute_canada.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-task=1
+#SBATCH --cpus-per-task=12
+#SBATCH --mem=128G
+#SBATCH --time=2:00:00
+#SBATCH --output=/home/j46lei/projects/rrg-dclausi/j46lei/IC-Light/output/%u_%x_%j.log
+#SBATCH --account=rrg-dclausi
+#SBATCH --mail-user=junf137@outlook.com
+#SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=REQUEUE
+
+
+# Usage: sbatch train_infer.sh <config> <wandb_project> <seed> <venv_path>
+if [ "$#" -ne 4 ]; then
+    error "Usage: sbatch ${0##*/} <config> <wandb_project> <seed> <venv_path>"
+    exit 1
+fi
+
+# Purge all loaded modules
+echo "Purging all loaded modules..."
+module --force purge
+
+# Load necessary modules
+echo "Loading required modules..."
+module load StdEnv gcc opencv
+module load python/3.10.13
+
+
+# Activate the virtual environment
+source $4/bin/activate
+
+echo "Running the python script..."
+python gradio_demo.py
