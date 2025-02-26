57697cb9694910d40320366110a09ea409be43dc
Junfeng Lei <3086638517@qq.com>
2025-02-26 11:25:10 -0500

    [feat] update compute_canada.sh
    1. change required ram and log location
    2. better parameters check
    3. fix venv activation issue

--- compute_canada.sh
+++ compute_canada.sh
@@ -3,9 +3,9 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-task=1
 #SBATCH --cpus-per-task=12
-#SBATCH --mem=128G
+#SBATCH --mem=64G
 #SBATCH --time=2:00:00
-#SBATCH --output=/home/j46lei/projects/rrg-dclausi/j46lei/IC-Light/output/%u_%x_%j.log
+#SBATCH --output=/home/j46lei/projects/rrg-dclausi/j46lei/IC-Light/output/log/%u_%x_%j.log
 #SBATCH --account=rrg-dclausi
 #SBATCH --mail-user=junf137@outlook.com
 #SBATCH --mail-type=BEGIN
@@ -13,12 +13,18 @@
 #SBATCH --mail-type=FAIL
 #SBATCH --mail-type=REQUEUE
 
-
-# Usage: sbatch train_infer.sh <config> <wandb_project> <seed> <venv_path>
-if [ "$#" -ne 4 ]; then
-    error "Usage: sbatch ${0##*/} <config> <wandb_project> <seed> <venv_path>"
+# check parameters
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <env_path>"
     exit 1
 fi
+
+env_path=$1
+# remove if the last char in env_path is "/"
+if [ "${env_path: -1}" == "/" ]; then
+    env_path=${env_path::-1}
+fi
+
 
 # Purge all loaded modules
 echo "Purging all loaded modules..."
@@ -29,9 +35,8 @@ echo "Loading required modules..."
 module load StdEnv gcc opencv
 module load python/3.10.13
 
-
 # Activate the virtual environment
-source $4/bin/activate
+source $env_path/bin/activate
 
 echo "Running the python script..."
 python gradio_demo.py
